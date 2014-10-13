aed6f2bc35690d7900812795c3ebef553c35513c
gntics <alexandre.quard@gmail.com>
2014-10-13 02:10:17 +0200

    update WeatherIcons to v1.2

--- app/scripts/helpers/helpers.js
+++ app/scripts/helpers/helpers.js
@@ -16,24 +16,24 @@ Ember.Handlebars.helper('format-temp', function(temp) {
 
 Ember.Handlebars.helper('iconW', function(icon) {
   var iconMap = {
-    "01d": "<i class='wi-day-sunny'></i>",
-    "01n": "<i class='wi-night-clear'></i>",
-    "02d": "<i class='wi-day-cloudy'></i>",
-    "02n": "<i class='wi-night-cloudy'></i>",
-    "03d": "<i class='wi-cloudy'></i>",
-    "03n": "<i class='wi-cloudy'></i>",
-    "04d": "<i class='wi-cloudy'></i>",
-    "04n": "<i class='wi-cloudy'></i>",
-    "09d": "<i class='wi-showers'></i>",
-    "09n": "<i class='wi-showers'></i>",
-    "10d": "<i class='wi-day-rain'></i>",
-    "10n": "<i class='wi-night-rain'></i>",
-    "11d": "<i class='wi-thunderstorm'></i>",
-    "11n": "<i class='wi-thunderstorm'></i>",
-    "13d": "<i class='wi-day-snow'></i>",
-    "13n": "<i class='wi-night-snow'></i>",
-    "50d": "<i class='wi-fog'></i>",
-    "50n": "<i class='wi-fog'></i>"
+    "01d": "<i class='wi wi-day-sunny'></i>",
+    "01n": "<i class='wi wi-night-clear'></i>",
+    "02d": "<i class='wi wi-day-cloudy'></i>",
+    "02n": "<i class='wi wi-night-cloudy'></i>",
+    "03d": "<i class='wi wi-cloudy'></i>",
+    "03n": "<i class='wi wi-cloudy'></i>",
+    "04d": "<i class='wi wi-cloudy'></i>",
+    "04n": "<i class='wi wi-cloudy'></i>",
+    "09d": "<i class='wi wi-showers'></i>",
+    "09n": "<i class='wi wi-showers'></i>",
+    "10d": "<i class='wi wi-day-rain'></i>",
+    "10n": "<i class='wi wi-night-rain'></i>",
+    "11d": "<i class='wi wi-thunderstorm'></i>",
+    "11n": "<i class='wi wi-thunderstorm'></i>",
+    "13d": "<i class='wi wi-day-snow'></i>",
+    "13n": "<i class='wi wi-night-snow'></i>",
+    "50d": "<i class='wi wi-fog'></i>",
+    "50n": "<i class='wi wi-fog'></i>"
   };
   return new Handlebars.SafeString(iconMap[icon]);
 });
