b927669174f1fa364e5f2055aa3977ead2fb1f93
gntics <alexandre.quard@gmail.com>
2014-10-14 15:57:04 +0200

    tmp changes to fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,9 +163,10 @@ module.exports = function (grunt) {
                 files: {
                     src: [
                         '<%= yeoman.dist %>/scripts/{,*/}*.js',
-                        '<%= yeoman.dist %>/styles/{,*/}*.css',
-                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
-                        '<%= yeoman.dist %>/styles/fonts/*'
+                        '<%= yeoman.dist %>/styles/{,*/}*.css'
+                        //,
+                        //'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                        //'<%= yeoman.dist %>/styles/fonts/*'
                     ]
                 }
             }
