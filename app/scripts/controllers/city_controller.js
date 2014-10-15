870f45cd955b8f36c699a90b94648b3b381fce82
gntics <alexandre.quard@gmail.com>
2014-10-15 20:19:40 +0200

    find or create city if no record found

--- app/scripts/controllers/city_controller.js
+++ app/scripts/controllers/city_controller.js
@@ -2,14 +2,27 @@ EmberWeather.CityController = Ember.ObjectController.extend({
   actions: {
     createCity: function(){
       var city = this.get('model');
-      var cityRecord = this.store.createRecord('city', {
-        formatted_location: city.formatted_address,
-        lat: city.geometry.location.lat,
-        lng: city.geometry.location.lng
-      });
-      cityRecord.save();
+      var str = this.get('store');
+      var context = this;
 
-      this.transitionToRoute('forecasts', cityRecord);
+      var cityRecord = this.store.find('city', {formatted_location: city.formatted_address});
+
+      cityRecord.then(fulfill, reject);
+
+      function fulfill(answer) {
+        tmp = answer.get('content').get('firstObject').id;
+        context.transitionToRoute('forecasts', tmp);
+      }
+
+      function reject(reason) {
+        cityRecord = str.createRecord('city', {
+          formatted_location: city.formatted_address,
+          lat: city.geometry.location.lat,
+          lng: city.geometry.location.lng
+        });
+        cityRecord.save();
+        context.transitionToRoute('forecasts', cityRecord);
+      }
     }
   }
 });
